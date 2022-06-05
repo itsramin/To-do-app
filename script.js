7824c8a68f83e15b79a7263516206c94851812c5
Ramin <raminshakooei@gmail.com>
2022-06-05 18:20:45 +0430

    style in progress

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 "use strict";
-const taskText = document.querySelector(".task--text");
-const taskList = document.querySelector(".tasks--list");
-const taskUndoneList = document.querySelector(".tasks--undone--list");
-const taskUndone = document.querySelector(".tasks--undone");
-const taskDoneList = document.querySelector(".tasks--done--list");
-const taskDone = document.querySelector(".tasks--done");
-let checkboxs = document.querySelectorAll(".checkbox");
-const btnSubTask = document.querySelector(".sub--task");
-const del = document.querySelector(".del");
-const deltask = document.querySelector(".deltask");
-const calendar = document.querySelector(".calendar");
-const taskdate = document.querySelector(".taskdate");
+
+const tabs = document.querySelector(".tabs");
+const btnNew = document.querySelector(".btn--new");
+const tasksLists = document.querySelector(".tasks--lists");
+const formNew = document.querySelector(".new--task--form");
+const listGroup = document.querySelector(".list-group");
 
 class App {
   constructor() {}
 }
 
 const app = new App();
+
+const displayNewForm = function () {
+  tasksLists.classList.add("hidden");
+  tabs.classList.add("hidden");
+  listGroup.classList.add("hidden");
+  formNew.classList.remove("hidden");
+};
+
+tabs.addEventListener("click", function (e) {
+  // active current tab
+  document
+    .querySelectorAll(".tab")
+    .forEach((tab) => tab.classList.remove("tab--active"));
+  e.target.classList.add("tab--active");
+
+  // show active list
+  document
+    .querySelectorAll(".tasks--list")
+    .forEach((list) => list.classList.toggle("hidden"));
+});
+
+btnNew.addEventListener("click", displayNewForm);
