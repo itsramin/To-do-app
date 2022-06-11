2352d2db11f2ff94051bfc4f02c6ee47c58c93d0
Ramin <raminshakooei@gmail.com>
2022-06-11 18:43:01 +0430

    incomplete

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticTodo = "dev-Todo-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticTodo).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
