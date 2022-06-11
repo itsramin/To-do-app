7803354cc1dba186894e91caf8e548ef52e840da
Ramin <raminshakooei@gmail.com>
2022-06-11 21:56:55 +0430

    bug fix

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,3 +8,10 @@ self.addEventListener("install", (installEvent) => {
     })
   );
 });
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
