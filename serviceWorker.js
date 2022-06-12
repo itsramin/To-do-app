e28cce2c9ec19f787dd810717f6dfde3948cf3cd
Ramin <raminshakooei@gmail.com>
2022-06-12 22:01:30 +0430

    fix

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,20 +1,20 @@
-// const staticTodo = "dev-todo-site-v1";
-// const assets = ["/", "/index.html", "/style.css", "/script.js", "/accept.mp3"];
+const staticTodo = "dev-todo-site-v1";
+const assets = ["/", "/index.html", "/style.css", "/script.js", "/accept.mp3"];
 
-// self.addEventListener("install", (installEvent) => {
-//   installEvent.waitUntil(
-//     caches.open(staticTodo).then((cache) => {
-//       cache.addAll(assets);
-//     })
-//   );
-// });
-// self.addEventListener("fetch", (fetchEvent) => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then((res) => {
-//       return res || fetch(fetchEvent.request);
-//     })
-//   );
-// });
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticTodo).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
 var GHPATH = "/To-do-app";
 
 // Choose a different app prefix name
@@ -35,4 +35,4 @@ var URLS = [
   `${GHPATH}/script.js`,
 ];
 
-self.addEventListener("fetch", function (event) {});
+// self.addEventListener("fetch", function (event) {});
