dd4e121c4e095c256818b555de8734958dec8d1c
Ramin <raminshakooei@gmail.com>
2022-06-18 19:41:26 +0430

    add readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Todo app
+
+A simple todo application without any dependencies, working with html, css, javascript.
+see live [demo](https://itsramin.github.io/To-do-app/).
+
+## Features
+
+- pwa
+- due date
+- dark mode
+- categories
+- sort by time
+- show missed tasks
+- responsive interface
+- repeatation interval
+- search between all tasks
+- save data in local storage
+- show finised and unfinished tasks count
--- script.js
+++ script.js
@@ -888,17 +888,4 @@ if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/To-do-app/serviceWorker.js");
 }
 
-// // ios
-// // Detects if device is on iOS
-// const isIos = () => {
-//   const userAgent = window.navigator.userAgent.toLowerCase();
-//   return /iphone|ipad|ipod/.test(userAgent);
-// };
-// // Detects if device is in standalone mode
-// const isInStandaloneMode = () =>
-//   "standalone" in window.navigator && window.navigator.standalone;
 
-// // Checks if should display install popup notification:
-// if (isIos() && !isInStandaloneMode()) {
-//   this.setState({ showInstallMessage: true });
-// }
