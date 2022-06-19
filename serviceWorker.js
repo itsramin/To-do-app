aed3a1b6e8642ed65fe3fe170ff963eb412bf28f
Ramin <raminshakooei@gmail.com>
2022-06-19 19:36:25 +0430

    service worker change to back

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,67 +6,68 @@ const contentToCache = [
   "/To-do-app/reset.css",
   "/To-do-app/style.css",
   "/To-do-app/media/accept.mp3",
+  "/To-do-app/icon/gcal.png",
   "/To-do-app/icon/icon-144.png",
 ];
 
 const RUNTIME = "runtime";
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => cache.addAll(contentToCache))
-      .then(self.skipWaiting())
-  );
-});
+// // The install handler takes care of precaching the resources we always need.
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches
+//       .open(cacheName)
+//       .then((cache) => cache.addAll(contentToCache))
+//       .then(self.skipWaiting())
+//   );
+// });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", (event) => {
-  const currentCaches = [cacheName, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+// // The activate handler takes care of cleaning up old caches.
+// self.addEventListener("activate", (event) => {
+//   const currentCaches = [cacheName, RUNTIME];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return cacheNames.filter(
+//           (cacheName) => !currentCaches.includes(cacheName)
+//         );
+//       })
+//       .then((cachesToDelete) => {
+//         return Promise.all(
+//           cachesToDelete.map((cacheToDelete) => {
+//             return caches.delete(cacheToDelete);
+//           })
+//         );
+//       })
+//       .then(() => self.clients.claim())
+//   );
+// });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener("fetch", (event) => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+// // The fetch handler serves responses for same-origin resources from a cache.
+// // If no response is found, it populates the runtime cache with the response
+// // from the network before returning it to the page.
+// self.addEventListener("fetch", (event) => {
+//   // Skip cross-origin requests, like those for Google Analytics.
+//   if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then((cachedResponse) => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+//         return caches.open(RUNTIME).then((cache) => {
+//           return fetch(event.request).then((response) => {
+//             // Put a copy of the response in the runtime cache.
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response;
+//             });
+//           });
+//         });
+//       })
+//     );
+//   }
+// });
 // self.addEventListener("install", (e) => {
 //   console.log("[Service Worker] Install");
 //   e.waitUntil(
