72a96c90db3b35df3d0d584d4289abfa93ecfe2e
Ramin <raminshakooei@gmail.com>
2022-07-02 14:39:27 +0430

    init

---
+++ serviceWorker.js
@@ -0,0 +1,38 @@
+const staticCacheName = "site-static-v1";
+const cacheAssets = [
+  "/",
+  "./index.html",
+  "./src/js/controller.js",
+  "./src/js/model.js",
+  "./src/js/helper.js",
+  "./src/css/reset.css",
+  "./src/css/style.css",
+  "./src/js/media/icon/icon-72.png",
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then((cache) => {
+        console.log("caching assets...");
+        cache.addAll(cacheAssets);
+      })
+      .catch((err) => {})
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then((res) => {
+        return res || fetch(evt.request);
+      })
+      .catch((err) => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("./pages/fallback.html");
+        }
+      })
+  );
+});
--- src/js/controller.js
+++ src/js/controller.js
@@ -215,3 +215,14 @@ const init = function () {
 };
 
 init();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./serviceWorker.js")
+    .then((reg) => {
+      console.log("Service worker registred successfully", reg);
+    })
+    .catch((err) => {
+      console.log("service worker not registred !!", err);
+    });
+}
